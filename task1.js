49814862443a1817498ca910c3b420780c7ba6dd
yash <rupaparayashu2006@gmail.com>
2024-06-23 19:30:24 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,20 @@
+
+function black(){
+    document.body.style.background="black"
+}
+
+function yellow(){
+    document.body.style.background="yellow"
+}
+
+function pink(){
+    document.body.style.background="pink"
+}
+
+function green(){
+    document.body.style.background="green"
+}
+
+function blue(){
+    document.body.style.background="blue"
+}
